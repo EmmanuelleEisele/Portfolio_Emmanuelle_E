ffd873318828ab88d2ba414f4e31d31ece03c272
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2025-09-16 10:57:37 +0200

    first commit : installations

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-green-500">
+        Hello Tailwind v3 🚀
+      </h1>
+    </div>
+  )
+}
