6de9a90ccd18890ae653d0883a7b041a974842aa
Emmanuelle Camarasa <manuecam2b@hotmail.fr>
2025-10-02 14:49:52 +0200

    adding a  futur projects data

--- src/data/futurProjects.js
+++ src/data/futurProjects.js
@@ -8,6 +8,13 @@ export const futurProjectsData = [
   },
   {
     id: 2,
+    title: "API To-Do List",
+    description: "Une API REST pour gérer des tâches avec authentification utilisateur.",
+    image: "/images/wip.png",
+    techno: ["Node.js", "Express", "MongoDB", "Mongoose", "JWT", "argon2"]
+  },
+  {
+    id: 3,
     title: "Site de e-commerce",
     description: "Un site de vente en ligne avec panier d'achat et paiement sécurisé.",
     image: "/images/wip.png",
